a0fcf1881a989510f5373c7f66fb0c34997cdb41
Cecabank <tpv@cecabank.es>
2020-01-29 11:39:46 +0100

    Changed version

--- wc_gateway_cecabank.php
+++ wc_gateway_cecabank.php
@@ -5,7 +5,7 @@
  * Description: Plugin de WooCommerce para conectar con la pasarela de Cecabank.
  * Author: Cecabank, S.A.
  * Author URI: https://www.cecabank.es/
- * Version: 0.1.3
+ * Version: 0.1.4
  * Text Domain: wc_cecabank
  * Domain Path: /i18n/languages/
  *
@@ -69,7 +69,7 @@ try {
  *
  * @class 		WC_Gateway_Cecabank
  * @extends		WC_Payment_Gateway
- * @version		0.1.3
+ * @version		0.1.4
  * @package		WooCommerce/Classes/Payment
  * @author 		Cecabank, S.A.
  */
